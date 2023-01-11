3c8523e21f4874cefa86d2e5c494bc8b14a6502c
jarod.cox <jarod.cox@credera.com>
2023-01-11 14:01:31 -0600

    Add POST Testing

--- test/HelloWorld.Test/FunctionTest.cs
+++ test/HelloWorld.Test/FunctionTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
@@ -17,13 +18,6 @@ namespace NEOGOVLambda.Tests
 {
     public class FunctionTest
     {
-        private readonly Function _neogovLambda = new Function();
-        private readonly Mock<IAmazonDynamoDB> _mockDB = new Mock<IAmazonDynamoDB>();
-        private readonly APIGatewayProxyRequest _apiGatewayProxyRequest = new APIGatewayProxyRequest();
-        private readonly Mock<APIGatewayProxyRequest> _mock2 = new Mock<APIGatewayProxyRequest>();
-        private readonly Mock<Function> _mockFunction = new Mock<Function>();
-        private readonly Mock<AmazonDynamoDBClient> _mockFunction2 = new Mock<AmazonDynamoDBClient>();
-
         [Fact]
         public async Task TestGet_Good()
         {
@@ -41,26 +35,28 @@ namespace NEOGOVLambda.Tests
                     {
                         StatusCode = 200,
                         Body = "K9 Added"
-                    }; 
+                    };
                 }
-                
-                
+
+
                 var cls = mock.Create<Function>();
-                
+
                 IDictionary<string, string> parameters = new Dictionary<string, string>();
                 parameters.Add("K9_Id", "123");
-                
-                _apiGatewayProxyRequest.QueryStringParameters = parameters;
-                Task<APIGatewayProxyResponse> apiGatewayProxyResponseActual = cls.Get(_apiGatewayProxyRequest);
-                
-                
-                Assert.Equal(apiGatewayProxyResponseActual.Result.StatusCode, apiGatewayProxyResponseExpected.StatusCode);
+
+                APIGatewayProxyRequest apiGatewayProxyRequest = new APIGatewayProxyRequest();
+                apiGatewayProxyRequest.QueryStringParameters = parameters;
+                Task<APIGatewayProxyResponse> apiGatewayProxyResponseActual = cls.Get(apiGatewayProxyRequest);
+
+
+                Assert.Equal(apiGatewayProxyResponseActual.Result.StatusCode,
+                    apiGatewayProxyResponseExpected.StatusCode);
 
             }
-            
+
 
         }
-        
+
         [Fact]
         public async Task TestGet_Invalid()
         {
@@ -78,62 +74,64 @@ namespace NEOGOVLambda.Tests
                     {
                         Body = "Invalid K9",
                         StatusCode = 404
-                    }; 
+                    };
                 }
-                
-                
+
+
                 var cls = mock.Create<Function>();
-                
+
                 IDictionary<string, string> parameters = new Dictionary<string, string>();
                 parameters.Add("K9_Id", "123");
-                
-                _apiGatewayProxyRequest.QueryStringParameters = parameters;
-                Task<APIGatewayProxyResponse> apiGatewayProxyResponseActual = cls.Get(_apiGatewayProxyRequest);
-                
-                
-                Assert.NotEqual(apiGatewayProxyResponseActual.Result.StatusCode, apiGatewayProxyResponseExpected.StatusCode);
+
+                APIGatewayProxyRequest apiGatewayProxyRequest = new APIGatewayProxyRequest();
+                apiGatewayProxyRequest.QueryStringParameters = parameters;
+                Task<APIGatewayProxyResponse> apiGatewayProxyResponseActual = cls.Get(apiGatewayProxyRequest);
+
+
+                Assert.NotEqual(apiGatewayProxyResponseActual.Result.StatusCode,
+                    apiGatewayProxyResponseExpected.StatusCode);
 
             }
-            
+
 
         }
-        
+
         [Fact]
-        public async Task TestPost_Good()
-        {
+         public async Task TestPost_Good()
+         {
+        
+             using (var mock = AutoMock.GetLoose())
+             {
 
-            using (var mock = AutoMock.GetLoose())
-            {
-                mock.Mock<IDynamoDBContext>().Setup(x => x.LoadAsync<Function.K9s>("123", CancellationToken.None))
-                    .ReturnsAsync(GetK9("123"));
+                 var k9 = GetK9("123");//JsonSerializer.Deserialize<Function.K9s>(apiGatewayProxyRequest.Body);
 
-                var mockedK9 = GetK9("123");
-                APIGatewayProxyResponse apiGatewayProxyResponseExpected = new APIGatewayProxyResponse();
-                if (mockedK9 != null)
-                {
-                    apiGatewayProxyResponseExpected = new APIGatewayProxyResponse()
-                    {
-                        StatusCode = 200,
-                        Body = "K9 Added"
-                    }; 
-                }
-                
-                
-                var cls = mock.Create<Function>();
-                
-                IDictionary<string, string> parameters = new Dictionary<string, string>();
-                parameters.Add("K9_Id", "123");
-                
-                _apiGatewayProxyRequest.QueryStringParameters = parameters;
-                Task<APIGatewayProxyResponse> apiGatewayProxyResponseActual = cls.Get(_apiGatewayProxyRequest);
-                
-                
-                Assert.Equal(apiGatewayProxyResponseActual.Result.StatusCode, apiGatewayProxyResponseExpected.StatusCode);
+                 mock.Mock<IDynamoDBContext>().Setup(x => x.SaveAsync(k9, CancellationToken.None));
+                     
+                 APIGatewayProxyResponse apiGatewayProxyResponseExpected = new APIGatewayProxyResponse();
+                 if (k9 != null)
+                 {
+                     apiGatewayProxyResponseExpected = new APIGatewayProxyResponse()
+                     {
+                         StatusCode = 200,
+                         Body = "K9 Added"
+                     }; 
+                 }
+                 
+                 
+                 var cls = mock.Create<Function>();
 
-            }
-            
+                 APIGatewayProxyRequest apiGatewayProxyRequest = new APIGatewayProxyRequest();
+                 apiGatewayProxyRequest.Body = "{\"K9_Id\":\"123\",\"HandlerName\":\"Evan\",\"Name\":\"Benny\"}";
 
-        }
+                 Task<APIGatewayProxyResponse> apiGatewayProxyResponseActual = cls.Post(apiGatewayProxyRequest);
+                 
+                 
+                 Assert.Equal(apiGatewayProxyResponseActual.Result.StatusCode, apiGatewayProxyResponseExpected.StatusCode);
+        
+             }
+             
+        
+         }
 
         private Function.K9s GetK9(string check)
         {
@@ -144,6 +142,7 @@ namespace NEOGOVLambda.Tests
             {
                 return null;
             }
+
             return testK9;
         }
     }
